00a27d62c5d0cfbead7383069f5e20bfdde71819
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-01-04 15:35:24 +0800

    Add files via upload

---
+++ leetcode_no605_种花问题_w1.cpp
@@ -0,0 +1,79 @@
+﻿/* ------------------------------------------------------------------|
+假设有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+
+给你一个整数数组  flowerbed 表示花坛，由若干 0 和 1 组成，其中 0 表示没种植花，1 表示种植了花。另有一个数 n ，能否在不打破种植规则的情况下种入 n 朵花？能则返回 true ，不能则返回 false。
+
+ 
+
+示例 1：
+
+输入：flowerbed = [1,0,0,0,1], n = 1
+输出：true
+示例 2：
+
+输入：flowerbed = [1,0,0,0,1], n = 2
+输出：false
+ 
+
+提示：
+
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] 为 0 或 1
+flowerbed 中不存在相邻的两朵花
+0 <= n <= flowerbed.length
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/can-place-flowers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	
+*
+*	执行用时：36 ms, 在所有 C++ 提交中击败了90.06%的用户
+*	内存消耗：20.2 MB, 在所有 C++ 提交中击败了60.77%%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+    if (!n) {
+        return true;
+    }
+    int len = flowerbed.size();
+    for (int i = 0; i < len; i++) {
+        if (!n) {
+            return true;
+        }
+        if (!flowerbed[i] && (i + 1 == flowerbed.size() || !flowerbed[i + 1])) {
+            n--;
+            i++;
+        }
+        else if(flowerbed[i]){
+            i++;
+        }
+    }
+    return n ? false : true;
+}
